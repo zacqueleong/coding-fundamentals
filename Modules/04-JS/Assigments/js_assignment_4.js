0ea0e0938fb54bed0467a9c3eb027a4616715d21
Zacque <107402654+zacqueleong@users.noreply.github.com>
2023-05-26 01:10:05 +0800

    Add files via upload

---
+++ Modules/04-JS/Assigments/js_assignment_4.js
@@ -0,0 +1,9 @@
+function tetra(n){  
+    // your code
+    return (n * (n + 1) * (n + 2)) / 6;
+}
+
+//Test cases
+console.log(tetra(2) === 4)
+console.log(tetra(5) === 35)
+console.log(tetra(6) === 56)
