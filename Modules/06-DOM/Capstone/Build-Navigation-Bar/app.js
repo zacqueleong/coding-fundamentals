0d2a6c6c9e54c241c414bd5761bd41de61e17fbf
Zacque <107402654+zacqueleong@users.noreply.github.com>
2023-06-10 01:20:54 +0800

    Add files via upload

---
+++ Modules/06-DOM/Capstone/Build-Navigation-Bar/app.js
@@ -0,0 +1,38 @@
+// Select the header element
+let navBar = document.querySelector('.header');
+
+// Event listener: Mouse move                
+// When user hovers around the top area, show the header   
+window.addEventListener("mousemove", function (e) {
+    if (e.clientY < 120) {
+        showNav();
+    }
+});
+
+// Event listener: Scroll                
+
+window.addEventListener("scroll", function (e) {
+    // Restore header when user scroll back to the very top.
+    if (window.scrollY === 0) {
+        showNav();
+    } else {
+        // When user scroll after a certain height, hide the header, else show the header
+        if (window.scrollY > 5) {
+            hideNav();
+        } else {
+            showNav();
+        }
+    }
+});
+
+// - Use window.scrollY to check the scroll height of the user on the website.
+// - Use scroll event for tracking the scrolling movement of user.
+
+// Functions
+function hideNav() {
+    navBar.classList.add('hide');
+}
+
+function showNav() {
+    navBar.classList.remove('hide');
+}
